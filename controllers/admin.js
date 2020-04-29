a28cd3bf9cfc5293e914d2751bd8cb85f027bdbf
khaled45 <khaledkamal9734@gmail.com>
2020-04-29 17:20:15 +0200

    documentation of posting and admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,124 +11,236 @@ const multer = require("multer");
 const path = require("path");
 var bcrypt = require("bcryptjs");
 var {
-    validateauthor,
-    author
-  } = require("../models/author");
+  validateauthor,
+  author
+} = require("../models/author");
+var {
+  validateadmin,
+  admin
+} = require("../models/admin");
+var {
+  validateuser,
+  user
+} = require("../models/user");
+
+/**
+* @swagger
+* /xlarge/admin/add/author:
+*  post:
+*    description: Use to sign up the users
+*    parameters:
+*     - name: email
+*       description: "Email of  new Author"
+*       required: true
+*       schema:
+*          type: string
+*          format: string 
+*     - name: password
+*       description: "Password of new Author"
+*       required: true
+*       schema:
+*          type: string
+*          format: string 
+*     - name: name
+*       description: "Name of new Author"
+*       required: true
+*       schema:
+*          type: string
+*          format: string 
+*    responses:
+*      '400':
+*        description: something went wrong || author already registered
+*      '200':
+*        description: A successful request with the data of this new author send in json format
+* 
+*/
+
+router.post("/add/author", parseUrlencoded, async (req, res, next) => {
+
   var {
-    validateadmin,
-    admin
-  } = require("../models/admin");
+    error
+  } = validateauthor(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  let authorr = await author.findOne({
+    email: req.body.email
+  });
+  if (authorr) {
+    return res.status(400).send("author already registered.");
+  }
+
+  authorr = new author({
+
+    email: req.body.email,
+    password: req.body.password,
+    name: req.body.name
+
+  });
+
+  var salt = await bcrypt.genSalt(10);
+  authorr.password = await bcrypt.hash(authorr.password, salt);
+  await authorr.save();
+
+
+  res.status(200).json({
+    authorr
+  });
+
+
+
+})
+
+
+  /**
+ * @swagger
+ * /xlarge/admin/delete/user/:id:
+ *  delete:
+ *    description: Use to delete post
+ *    responses:
+ *      '200':
+ *        description: user is deleted successfully
+ * 
+ */
+
+
+
+router.delete("/delete/user/:id", function (req, resp) {
+
+  mongoose.model("user").findOneAndRemove({
+    _id: req.params.id
+  },
+    function (err, data) {
+      if (!err) {
+      }
+    })
+
+  resp.json("user deleted")
+})
+
+  /**
+ * @swagger
+ * /xlarge/admin/delete/author/:id:
+ *  delete:
+ *    description: Use to delete post
+ *    responses:
+ *      '200':
+ *        description: author is deleted successfully
+ * 
+ */
+
+router.delete("/delete/author/:id", function (req, resp) {
+
+  mongoose.model("author").findOneAndRemove({
+    _id: req.params.id
+  },
+    function (err, data) {
+      if (!err) {
+      }
+    })
+
+  resp.json("author deleted")
+})
+
+
+  /**
+ * @swagger
+ * /xlarge/admin/add/admin:
+ *  post:
+ *    description: Use to Add new Admin
+ *    parameters:
+ *    -  in: body
+ *       name: body
+ *       description: "Add new Admin"
+ *       required: true
+ *       schema:
+ *         $ref: "#/definitions/admin"
+ *    responses:
+ *      '200':
+ *        description: A successful request with the data of this new Admin send in json format
+ * 
+ */
+
+
+router.post("/add/admin", parseUrlencoded, async (req, res) => {
   var {
-    validateuser,
-    user
-  } = require("../models/user");
-  
-  router.post("/add/author", parseUrlencoded, async (req, res, next) => {
+    error
+  } = validateadmin(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
 
-    var {
-        error
-      } = validateauthor(req.body);
-      if (error) {
-        return res.status(400).send(error.details[0].message);
-      }
-      let authorr = await author.findOne({
-        email: req.body.email
-      });
-      if (authorr) {
-        return res.status(400).send("author already registered.");
-      }
+  let new_admin = new admin({
+    password: req.body.password,
+    email: req.body.email,
 
-      authorr = new author({
-     
-      email: req.body.email,
-      password: req.body.password,
-      name:req.body.name
-     
-    });
-  
-    var salt = await bcrypt.genSalt(10);
-    authorr.password = await bcrypt.hash(authorr.password, salt);
-    await authorr.save();
-  
-    
-    res.status(200).json({
-        authorr
-    });
-
-
-
-  })
-
-  router.delete("/delete/user/:id", function (req, resp) {
-
-    mongoose.model("user").findOneAndRemove({
-        _id: req.params.id
-      },
-      function (err, data) {
-        if (!err) {
-        }
-      })
-  
-    resp.json("user deleted")
-  })
-  
-  router.delete("/delete/author/:id", function (req, resp) {
-
-    mongoose.model("author").findOneAndRemove({
-        _id: req.params.id
-      },
-      function (err, data) {
-        if (!err) {
-        }
-      })
-  
-    resp.json("author deleted")
-  })
-
-
-
-  router.post("/add/admin", parseUrlencoded, async (req, res) => {
-    var {
-      error
-    } = validateadmin(req.body);
-    if (error) {
-      return res.status(400).send(error.details[0].message);
-    }
-  
-    let new_admin = new admin({
-      password: req.body.password,
-      email: req.body.email,
-  
-    });
-    var salt = await bcrypt.genSalt(5);
-    new_admin.password = await bcrypt.hash(new_admin.password, salt);
-    await new_admin.save();
-    res.json(new_admin);
   });
-  
+  var salt = await bcrypt.genSalt(5);
+  new_admin.password = await bcrypt.hash(new_admin.password, salt);
+  await new_admin.save();
+  res.json(new_admin);
+});
 
-  router.get("/account/:id", async (req, res) => {
+  /**
+ * @swagger
+ * /xlarge/admin/account/:id:
+ *  get:
+ *    description: Use to find Admin Account 
+ *    responses:
+ *      '200':
+ *        description: A successful request with the data of  Admin send in json format
+ * 
+ */
 
-    let adminspec = await admin.findOne({
-      _id: req.params.id
-    });
-  
-    res.json(adminspec)
+router.get("/account/:id", async (req, res) => {
+
+  let adminspec = await admin.findOne({
+    _id: req.params.id
   });
-  
-  router.get("/author/list",async(req,res)=>{
-    let result = await   author.find({});
+
+  res.json(adminspec)
+});
+
+
+  /**
+ * @swagger
+ * /xlarge/admin/author/list:
+ *  get:
+ *    description: Use to retrieve All Authors  
+ *    responses:
+ *      '200':
+ *        description: A successful request with the data of all authors send in json format
+ * 
+ */
+
+
+router.get("/author/list", async (req, res) => {
+  let result = await author.find({});
   res.json(result)
-  
-  
-  })
-  router.get("/user/list",async(req,res)=>{
-    let result = await   user.find({});
+
+
+})
+
+
+  /**
+ * @swagger
+ * /xlarge/admin/user/list:
+ *  get:
+ *    description: Use to retrieve All Users  
+ *    responses:
+ *      '200':
+ *        description: A successful request with the data of all users send in json format
+ * 
+ */
+
+
+router.get("/user/list", async (req, res) => {
+  let result = await user.find({});
   res.json(result)
-  
-  
-  
-  
-  
-  })
-  module.exports = router;
+
+
+
+
+
+})
+module.exports = router;
--- controllers/posting.js
+++ controllers/posting.js
@@ -15,8 +15,25 @@ var {
   post
 } = require("../models/post");
 
+  /**
+ * @swagger
+ * /xlarge/post/create:
+ *  post:
+ *    description: Use to create new post
+ *    parameters:
+ *    -  in: body
+ *       name: body
+ *       description: "create new post"
+ *       required: true
+ *       schema:
+ *         $ref: "#/definitions/post"
+ *    responses:
+ *      '200':
+ *        description: A successful request with the data of this new post send in json format
+ * 
+ */
 
-router.post("/posting/create", parseUrlencoded, async (req, res) => {
+router.post("/create", parseUrlencoded, async (req, res) => {
   const { title, content, category, createdby } = req.body
 
   const post1 = new post({
@@ -31,21 +48,76 @@ router.post("/posting/create", parseUrlencoded, async (req, res) => {
   let author1 = await author.findOne({ _id: createdby })
   author1.post.push(post1._id)
   fresult = await author1.save((err, data) => {
-    res.json({ result: data, error: err })
+    res.json({ result})
   })
 })
 
-router.post("posting/delete", parseUrlencoded, async (req, res) => {
-  const { _id } = req.body
-  let result = await post.remove({ _id: _id })
-  resp.json("post is deleted")
+  /**
+ * @swagger
+ * /xlarge/post/delete/post/:id:
+ *  delete:
+ *    description: Use to delete post
+ *       required: true
+ *    responses:
+ *      '200':
+ *        description: post is deleted successfully
+ * 
+ */
+
+router.delete("/delete/post/:id", function (req, resp) {
+
+  mongoose.model("post").findOneAndRemove({
+    _id: req.params.id
+  },
+    function (err, data) {
+      if (!err) {
+      }
+    })
+
+  resp.json("post deleted")
 })
 
-router.post("posting/update", parseUrlencoded, async (req, res) => {
+  /**
+ * @swagger
+ * /xlarge/post/update:
+ *  post:
+ *    description: Use to delete post
+ *    parameters:
+ *      - name: title
+ *        description: updated Title of post
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *      - name: content
+ *        description: updated content of post
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *      - name: category
+ *        description: updated category of this post 
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *      - name: post_id
+ *        description: post id which will update
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *    responses:
+ *      '200':
+ *        description: A successful request with the data of this updated post send in json format
+ * 
+ */
+
+router.post("/update", parseUrlencoded, async (req, res) => {
 
   const { post_id, title, content, category } = req.body
   let result = await post.findOneAndUpdate({ _id: post_id }, { title: title, content: content, category: category })
-  resp.json({ result: result })
+  res.json({ result})
 
 })
 
